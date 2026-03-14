89499eb2a897318abf6c6a52ca7cca900b26a7c9
sahilcodexx <sahil171759@gmail.com>
2026-03-14 02:30:00 +0000

    verce analytics added

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "@/components/ui/sonner";
@@ -83,6 +84,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Toaster/>
+        <Analytics />
       </body>
     </html>
   );
